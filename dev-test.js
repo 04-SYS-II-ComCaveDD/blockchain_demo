82b770e116cb27dd6272730742e1fa57ee757f48
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-08-30 10:11:12 +0000

    Blockchain / constructor() , addBlock()

--- dev-test.js
+++ dev-test.js
@@ -6,4 +6,7 @@ const Block = require("./block");
 // const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
 // console.log(block.toString());
 
-console.log(Block.genesis().toString());
+// console.log(Block.genesis().toString());
+
+const testBlock = Block.mineBlock(Block.genesis(),"Bank an Kasse 1000,-");
+console.log(testBlock.toString());
